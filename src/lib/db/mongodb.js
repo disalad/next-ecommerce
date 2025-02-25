76c710dc4e419780575fc6f4c600e0eaa7a3b9aa
Disala Yuthmira <damsasdisala@gmail.com>
2025-02-25 19:59:36 +0530

    fix: Configure Mongoose connection to connect to a specific database

--- src/lib/db/mongodb.js
+++ src/lib/db/mongodb.js
@@ -4,12 +4,15 @@ const dbConnect = async () => {
     if (mongoose.connection.readyState >= 1) return;
 
     const mongodbURI = process.env.MONGODB_URI;
+    const databaseName = process.env.MONGODB_DB;
 
     if (!mongodbURI) {
         throw new Error('MONGODB_URI is not defined');
     }
 
-    return mongoose.connect(mongodbURI, {});
+    return mongoose.connect(mongodbURI, {
+        dbName: databaseName,
+    });
 };
 
 export default dbConnect;
